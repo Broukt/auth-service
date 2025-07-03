68ba293baa2158f26a885a3a1307e1da183c70fc
Jairo Calcina <j.calcina.v@gmail.com>
2025-07-03 03:11:11 -0400

    feat: add function to get the role from the header authorization request

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,12 +1,27 @@
 const jwt = require("jsonwebtoken");
 const { jwtSecret, jwtExpiresIn } = require("../config/env");
 
-const signToken = (id) =>
-  jwt.sign({ id }, jwtSecret, { expiresIn: jwtExpiresIn });
+const signToken = (userId, userEmail, role) =>
+  jwt.sign({ userId, userEmail, role }, jwtSecret, { expiresIn: jwtExpiresIn });
 
 const verifyToken = (token) => jwt.verify(token, jwtSecret);
+
+const getRoleFromHeader = (req) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return null; // No token provided
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = verifyToken(token);
+    return payload.role; // Return the role from the token payload
+  } catch (err) {
+    return null; // Token is invalid or expired
+  }
+};
 
 module.exports = {
   signToken,
   verifyToken,
+  getRoleFromHeader,
 };
