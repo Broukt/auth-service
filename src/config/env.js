15a537545e5acf7296e2627dae10573cfdaf49e3
Jairo Calcina <j.calcina.v@gmail.com>
2025-07-03 03:01:28 -0400

    chore: modify environment variables

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 module.exports = {
   port: process.env.PORT || 4000,
+  env: process.env.NODE_ENV || "development",
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN,
-  rabbitUrl: process.env.RABBITMQ_URL || "amqp://localhost:5672",
-  userCreatedQueue: process.env.USER_CREATED_QUEUE || "user.created",
+  rabbitUrl: process.env.RABBITMQ_URL || "amqp://localhost",
+  userCreatedQueueAuth:
+    process.env.USER_CREATED_QUEUE_AUTH || "user.created.auth",
+  userUpdatedQueue: process.env.USER_UPDATED_QUEUE || "user.updated",
+  userDeletedQueueAuth:
+    process.env.USER_DELETED_QUEUE_AUTH || "user.deleted.auth",
 };
