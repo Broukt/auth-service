eed031097ef56a277d93e0884b3c320ea6d616fe
Jairo Calcina <j.calcina.v@gmail.com>
2025-07-03 03:08:30 -0400

    fix: fix messages sent to the message broker

--- src/rabbitmq/consumer.js
+++ src/rabbitmq/consumer.js
@@ -1,34 +1,55 @@
-const bcrypt = require("bcrypt");
 const prisma = require("../database/prisma");
 const { connectRabbit } = require("./connection");
-const { userCreatedQueue } = require("../config/env");
+const {
+  userCreatedQueueAuth,
+  userUpdatedQueue,
+  userDeletedQueueAuth,
+} = require("../config/env");
 
-async function consumeUserCreated() {
+async function consumeUserEvents() {
   const ch = await connectRabbit();
-  await ch.assertQueue(userCreatedQueue, { durable: true });
-  ch.consume(userCreatedQueue, async (msg) => {
+  await ch.assertQueue(userCreatedQueueAuth, { durable: true });
+  await ch.assertQueue(userUpdatedQueue, { durable: true });
+  await ch.assertQueue(userDeletedQueueAuth, { durable: true });
+
+  ch.consume(userCreatedQueueAuth, async (msg) => {
     try {
-      const { id, email, password } = JSON.parse(msg.content.toString());
-      if (!id || !email || !password) {
-        console.error("Invalid message format:", msg.content.toString());
-        return ch.ack(msg);
-      }
-      let exists = await prisma.authUser.findUnique({ where: { id } });
-      if (exists) {
-        console.log("User already exists:", id);
-        return ch.ack(msg);
-      }
-      exists = await prisma.authUser.findUnique({ where: { email } });
-      if (!exists) {
-        const hashed = await bcrypt.hash(password, 12);
-        await prisma.authUser.create({ data: { id, email, password: hashed } });
-      }
+      const { id, name, lastName, email, password, roleId } = JSON.parse(
+        msg.content.toString()
+      );
+      await prisma.user.create({
+        data: { id, name, lastName, email, password, roleId },
+      });
       ch.ack(msg);
     } catch (err) {
-      console.error("Failed to process user.created:", err);
+      console.error("Error processing user.created.auth:", err);
+      ch.nack(msg, false, false);
+    }
+  });
+
+  ch.consume(userUpdatedQueue, async (msg) => {
+    try {
+      const { id, email } = JSON.parse(msg.content.toString());
+      await prisma.user.update({ where: { id }, data: { email } });
+      ch.ack(msg);
+    } catch (err) {
+      console.error("Error processing user.updated:", err);
+      ch.nack(msg, false, false);
+    }
+  });
+
+  ch.consume(userDeletedQueueAuth, async (msg) => {
+    try {
+      const { id } = JSON.parse(msg.content.toString());
+      await prisma.user.delete({
+        where: { id },
+      });
+      ch.ack(msg);
+    } catch (err) {
+      console.error("Error processing user.deleted:", err);
       ch.nack(msg, false, false);
     }
   });
 }
 
-module.exports = { consumeUserCreated };
+module.exports = { consumeUserEvents };
